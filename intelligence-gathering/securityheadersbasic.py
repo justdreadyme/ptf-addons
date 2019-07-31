1a8dcbb9a0376da8a5dee86cfd9471e74979cce1
root <root@localhost.localdomain>
2019-07-31 13:31:27 +0200

    Test commit

---
+++ intelligence-gathering/securityheadersbasic.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+##########################################
+# Installation module for Security Headers
+##########################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Jussi-Pekka Erkkila"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update security headers"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/juerkkil/securityheaders.git"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="python"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="python"
+
+#COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="securityheadersbasic"
+
+# THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
+LAUNCHER="securityheadersbasic"
---
+++ intelligence-gathering/securityheadersfull.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+##################################################
+# Installation module for Security Headers Checker
+##################################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Koen Buyens"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update security headers checker"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/koenbuyens/securityheaders.git"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="python"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="python"
+
+#COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="securityheadersfull"
+
+# THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
+LAUNCHER="securityheadersfull"
