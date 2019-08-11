ec93aa0de27928ab1f8fed77711f596526bc5ced
root <root@localhost.localdomain>
2019-08-11 22:28:19 +0200

    Module update

---
+++ intelligence-gathering/onetwopunch-addon.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for onetwopunch
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Harold Rodriguez (superkojiman)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update onetwopunch - A unicornscan & nmap wrapper"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/superkojiman/onetwopunch"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="onetwopunch"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
+LAUNCHER="onetwopunch"
+
+# PREREQ INSTALL MODULES NEEDED FOR THIS TOOL TO WORK PROPERLY
+# TOOL_DEPEND="modules/vulnerability-analysis/nmap,modules/intelligence-gathering/unicornscan"
+
