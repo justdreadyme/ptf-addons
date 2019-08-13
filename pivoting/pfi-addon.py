d96e8546a41910bd9013b29783a3b0484627c0c2
root <root@localhost.localdomain>
2019-08-14 01:38:30 +0200

    Module updates

---
+++ pivoting/pfi-addon.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for pfi
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Stephen A. Ridley (s7ephen)"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update pfi ~ Port Forwarding Interceptor"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/s7ephen/pfi"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="pfi"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git,python"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git,python"
+
+# CREATE LAUNCHER
+LAUNCHER=""
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
