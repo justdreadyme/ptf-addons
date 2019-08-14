fb32c6b600e4376543e30be49b9ecd0218e344d9
root <root@localhost.localdomain>
2019-08-14 14:33:26 +0200

    Module updates

---
+++ pre-engagement/bookmarks-01-addon.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for Bookmarks 01 ~ Bookmarks Collection
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="trimstray"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update Bookmarks 01 ~ Bookmarks Collection"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/trimstray/the-book-of-secret-knowledge"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="bookmarks-01"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
+
+# THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
+LAUNCHER=""
+
