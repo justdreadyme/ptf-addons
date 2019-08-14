53b52fc03e5f598237a428d7eebeb6d6252903a6
root <root@localhost.localdomain>
2019-08-14 19:43:22 +0200

    Module updates

---
+++ intelligence-gathering/watson-addon.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for Watson
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Rasta Mouse"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="This module will install/update Watson - Privilege Escalation Checker via missing patches"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/rasta-mouse/Watson"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="watson"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA="git"
+
+# COMMANDS TO RUN AFTER
+# Compile with Visual Studio on MS Windows...
+AFTER_COMMANDS=""
+
+# create launcher
+LAUNCHER=""
