7ecd90d7453791e5752968e672601fed60acfdc5
justdreadyme <31898015+justdreadyme@users.noreply.github.com>
2025-08-12 16:58:02 +0200

    Update wpa2-halfhandshake-crack-addon.py
    Fix custom launcher.

--- wireless/wpa2-halfhandshake-crack-addon.py
+++ wireless/wpa2-halfhandshake-crack-addon.py
@@ -26,7 +26,7 @@ DEBIAN="git,python"
 FEDORA="git,python"
 
 # COMMANDS TO RUN AFTER
-AFTER_COMMANDS="cd {INSTALL_LOCATION},virtualenv venv,/bin/bash -c 'source {INSTALL_LOCATION}/venv/bin/activate && curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py && python2.7 get-pip.py && python2.7 -m pip install setuptools && python2.7 -m pip install pypcapfile && python2.7 -m pip install pbkdf2_ctypes && python2.7 setup.py install && deactivate',chown -R kali\:kali {INSTALL_LOCATION},chmod -R 0755 {INSTALL_LOCATION},echo '#!/bin/bash' > launcher,echo cd {INSTALL_LOCATION} >> launcher,echo source {INSTALL_LOCATION}/venv/bin/activate \&\& python2.7 halfHandshake.py \"\$\@\" \&\& deactivate >> launcher,chmod +x {INSTALL_LOCATION}/launcher,sudo ln -sf {INSTALL_LOCATION}/launcher /usr/local/bin/halfHandshake"
+AFTER_COMMANDS="cd {INSTALL_LOCATION},virtualenv venv,/bin/bash -c 'source {INSTALL_LOCATION}/venv/bin/activate && curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py && python2.7 get-pip.py && python2.7 -m pip install setuptools && python2.7 -m pip install pypcapfile && python2.7 -m pip install pbkdf2_ctypes && python2.7 setup.py install && deactivate',chown -R kali\:kali {INSTALL_LOCATION},chmod -R 0755 {INSTALL_LOCATION},echo '#!/bin/bash' > launcher,echo 'cd' '{INSTALL_LOCATION}' >> launcher,echo source {INSTALL_LOCATION}/venv/bin/activate \&\& python2.7 halfHandshake.py \"\$\@\" \&\& deactivate >> launcher,chmod +x {INSTALL_LOCATION}/launcher,sudo ln -sf {INSTALL_LOCATION}/launcher /usr/local/bin/halfHandshake"
 
 # THIS WILL CREATE AN AUTOMATIC LAUNCHER FOR THE TOOL
 # cd {INSTALL_LOCATION}, sudo python2 halfHandshake.py
